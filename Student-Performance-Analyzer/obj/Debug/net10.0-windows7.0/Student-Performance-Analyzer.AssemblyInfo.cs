e6eafde2028bbdcf21a286d40adfe09cb393c475
MashaTitova <masha.m.titova@yandex.ru>
2026-04-14 20:15:44 +0300

    added functions for charting

--- Student-Performance-Analyzer/obj/Debug/net10.0-windows7.0/Student-Performance-Analyzer.AssemblyInfo.cs
+++ Student-Performance-Analyzer/obj/Debug/net10.0-windows7.0/Student-Performance-Analyzer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Student-Performance-Analyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2aefcb2c909a27d17f6c6e4a90b8bd30f8313b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91af9eb1ef0c1292264cbecbbfc39515be1f1ce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Student-Performance-Analyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Student-Performance-Analyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
